9fd938b09b611fb8c55deb475d3cccfbb51e1793
Hongrui Liu <hongruil@umich.edu>
2023-04-09 16:26:35 -0400

    Complet camera component and JSON

--- client/src/components/Camera.vue
+++ client/src/components/Camera.vue
@@ -47,59 +47,68 @@
   </script>
    -->
 
-   <template>
-    <div>
-      <video v-if="photoTaken === false" ref="video" autoplay></video>
-      <button  @click="capture">Capture</button>
-      <canvas ref="canvas"></canvas>
-    </div>
-  </template>
-  
-  <script>
-  
-  export default {
-    name: "Camera",
-    data() {
-      return {
-        mediaStream: null,
-        photoTaken: false,
-        dataURL: null,
-      }
-    },
-    mounted() {
-      // Request access to the camera
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then(stream => {
-          // Set the mediaStream to the camera stream
-          this.mediaStream = stream;
-          // Set the video source to the camera stream
-          this.$refs.video.srcObject = stream;
-        })
-        .catch(err => {
-          console.log("Unable to access camera", err);
-        });
-    },
-    methods: {
-      capture() {
-        // Set photoTaken to true to show the captured photo
-        this.photoTaken = true;
-        // Pause the video stream
-        this.$refs.video.pause();
-        // Get the canvas element
-        const canvas = this.$refs.canvas;
-        // Set the canvas dimensions to match the video dimensions
-        canvas.width = this.$refs.video.videoWidth;
-        canvas.height = this.$refs.video.videoHeight;
-        // Draw the video frame onto the canvas
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(this.$refs.video, 0, 0, canvas.width, canvas.height);
-        // Stop the camera stream
-        this.mediaStream.getTracks()[0].stop();
-        this.dataURL = canvas.toDataURL();
-        localStorage.setItem('dataURL', this.dataURL);
-      }
+<template>
+  <div class="camera-container">
+    <video width="480" height="360" v-if="photoTaken === false" ref="video" autoplay></video>
+    <canvas width="0" height="0" ref="canvas"></canvas>
+    <br>
+    <el-button type="primary" @click="capture">Capture</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Camera",
+  data() {
+    return {
+      mediaStream: null,
+      photoTaken: false,
+      dataURL: null,
+      isCaptured: false,
+    }
+  },
+  mounted() {
+    // Request access to the camera
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        // Set the mediaStream to the camera stream
+        this.mediaStream = stream;
+        // Set the video source to the camera stream
+        this.$refs.video.srcObject = stream;
+      })
+      .catch(err => {
+        console.log("Unable to access camera", err);
+      });
+  },
+  // props: {
+  //   isCaptured: Boolean
+  // },
+  methods: {
+    capture() {
+      // Set photoTaken to true to show the captured photo
+      this.photoTaken = true;
+      // Pause the video stream
+      this.$refs.video.pause();
+      // Get the canvas element
+      const canvas = this.$refs.canvas;
+      // Set the canvas dimensions to match the video dimensions
+      // canvas.width = this.$refs.video.videoWidth;
+      // canvas.height = this.$refs.video.videoHeight;
+      canvas.width = '480';
+      canvas.height = '360';
+      // Draw the video frame onto the canvas
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(this.$refs.video, 0, 0, canvas.width, canvas.height);
+      // Stop the camera stream
+      this.mediaStream.getTracks()[0].stop();
+      this.dataURL = canvas.toDataURL();
+      localStorage.setItem('dataURL', this.dataURL);
+      this.isCaptured = true;
+      this.$emit('eventname', this.isCaptured)
+
     }
   }
+}
 
-  </script>
-  
+</script>
